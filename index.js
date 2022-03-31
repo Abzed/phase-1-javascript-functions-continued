010993fbd12796465b5b1169b257a66c89d0533c
Abzed <ibraabzed@gmail.com>
2022-03-31 14:42:24 +0300

    commit tree 6a1941d6f46d30a7d48b224304723e70436743d3

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// code your solution here
+function saturdayFun(txt='roller-skate'){
+    const str ='This Saturday, I want to'
+    return str + ' ' + txt + '!' 
+}
+console.log(saturdayFun('bathe my dog'));
+
+function mondayWork(act='go to the office'){
+    const str = 'This Monday, I will'
+    return str + ' ' + act + '.' 
+}
+console.log(mondayWork());
+
+
+
+function wrapAdjective(ast){
+    return function wrapper (adj){
+        const str = 'You are'
+        let ex = '!'
+        return `${str} ${ast}${adj}${ast}${ex}`;
+    }
+    wrapper('a hard worker')
+}
+console.log(wrapAdjective('*'));
+
+
+
+
+
+
+
+
